8c3c4db4d6fb5b4801fbb946015b5da394a04f58
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-08-08 11:03:57 +0530

    added solution

---
+++ CustomerBulkUpload/CustomerBulkUpload/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;   
+
+namespace CustomerBulkUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"<Your destination connection string>";
+            
+            DataSet ds = new DataSet();
+            DataTable sourceData = new DataTable();
+            ds.ReadXml(@"<enter path of BulkUploadOrders.xml>");
+            sourceData = ds.Tables[0];
+            int i = sourceData.Rows.Count;   
+
+            // open the destination data
+            using (SqlConnection destinationConnection =
+                            new SqlConnection(connectionString))
+            {
+                // open the connection
+                destinationConnection.Open();
+                using (SqlBulkCopy bulkCopy =
+                            new SqlBulkCopy(destinationConnection.ConnectionString,  SqlBulkCopyOptions.TableLock ))
+                {
+                    bulkCopy.SqlRowsCopied += new SqlRowsCopiedEventHandler(OnSqlRowsTransfer);
+                    bulkCopy.NotifyAfter = 100;
+                    bulkCopy.BatchSize = 50;
+                    bulkCopy.ColumnMappings.Add("OrderID", "NewOrderID");     
+                    bulkCopy.DestinationTableName = "TopOrders";
+                    bulkCopy.WriteToServer(sourceData);
+                }
+            }
+        }
+
+        private static void OnSqlRowsTransfer(object sender, SqlRowsCopiedEventArgs e)
+        {
+            Console.WriteLine("Copied {0} so far...", e.RowsCopied);
+        }
+    }
+}
